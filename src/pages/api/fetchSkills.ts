559b97691015ee53ef4120fa62a966da5a92ff96
Camille HAUSTANT <camillehaustant@gmail.com>
2023-01-19 17:36:31 +0100

    first commit

---
+++ src/pages/api/fetchSkills.ts
@@ -0,0 +1,16 @@
+import { sanityClient as client } from './sanityClient';
+
+import type { sanityRootType } from "@/types/SanityRootType";
+import type { fetchedImageType } from "@/types/fetchedImageType";
+
+export type fetchedSkillType = sanityRootType & {
+    skill: string;
+    image: fetchedImageType;
+}
+const fetchSkills = async () => {
+    const fetchedSkills: fetchedSkillType[] = await client.fetch(`*[_type == "skill"]{ skill, image{alt, asset->{url}}}`);
+
+    return fetchedSkills.map((fetchedSkill: fetchedSkillType) => ({ skill: fetchedSkill.skill, image: { alt: fetchedSkill.image.alt, url: fetchedSkill.image.asset.url } }));
+}
+
+export { fetchSkills };
