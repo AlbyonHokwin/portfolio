6c083a0281de56729e3c3c66e6bb58fe8a8785e8
Camille HAUSTANT <camillehaustant@gmail.com>
2023-01-20 14:04:26 +0100

    update of Me component

--- src/components/Me.tsx
+++ src/components/Me.tsx
@@ -1,15 +1,48 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from '@/styles/Me.module.css'
-import type { profileType } from '@/types/profileType'
+
+import type { imageType } from '@/types/imageType'
+import type { socialType } from '@/types/socialType'
 
 type propsType = {
-    profile: profileType;
+    picture: imageType;
+    pictureGit: imageType;
+    socials: socialType[];
 }
 
-function Me({ profile }: propsType) {
+function Me({ picture, pictureGit, socials }: propsType) {
+    const linkedIn = socials.filter(social => /linkedin/i.test(social.name))[0];
+    const github = socials.filter(social => /github/i.test(social.name))[0];
+
     return (
         <div className={styles.container}>
-            {profile.lastname}
+            <div className={styles.imageContainer}>
+                <Image
+                    style={{ objectFit: "cover", borderRadius: "50%" }}
+                    src={picture.url}
+                    alt={picture.alt}
+                    fill={true}
+                    sizes="30vw"
+                />
+            </div>
+            <a href={github ? github.url : '#'} className={styles.imageContainer} >
+                <Image
+                    style={{ objectFit: "cover", borderRadius: "50%" }}
+                    src={pictureGit.url}
+                    alt={pictureGit.alt}
+                    fill={true}
+                    sizes="30vw"
+                />
+                {github && <Image
+                    style={{ objectFit: "cover", borderRadius: "50%", /* backgroundColor: "gray", opacity: 0.8  */ }}
+                    className={styles.hoverImage}
+                    src={github.image.url}
+                    alt={github.image.alt}
+                    fill={true}
+                    sizes="30vw"
+                />}
+            </a>
         </div>
     )
 }
