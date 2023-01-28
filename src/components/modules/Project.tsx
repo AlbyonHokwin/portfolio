b697db9d69b952c0a7defd8da03518dd8a96bf4e
Camille HAUSTANT <camillehaustant@gmail.com>
2023-01-28 14:21:45 +0100

    add video to project component

--- src/components/modules/Project.tsx
+++ src/components/modules/Project.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from '@/styles/Project.module.css'
 import Image from 'next/image';
 import Skill from '@/components/elements/Skill';
@@ -11,6 +11,9 @@ type propsType = {
 }
 
 function Project({ project }: propsType) {
+    const imagesContainerRef = useRef<HTMLDivElement>(null);
+    const [imagesContainerHeight, setImagesContainerHeight] = useState<number>(0);
+
     const {
         projectTitle,
         description,
@@ -23,12 +26,28 @@ function Project({ project }: propsType) {
         skills,
     } = project;
 
+    useEffect(() => {
+        imagesContainerRef.current && setImagesContainerHeight(imagesContainerRef.current?.clientHeight)
+    }, [imagesContainerRef]);
+
     let dateStr: string = '';
     !onProgress && (dateStr = Intl.DateTimeFormat(undefined, { month: 'long', year: 'numeric' }).format(new Date(date)));
 
     return (
         <div className={styles.container}>
-            <div className={styles.images}>
+            <div className={styles.images} ref={imagesContainerRef}>
+                {video &&
+                    <div className={styles.videoContainer}>
+                        <iframe
+                            className={styles.video}
+                            src={video}
+                            allow="autoplay; fullscreen"
+                            loading='lazy'
+                            height={0.98 * imagesContainerHeight}
+                            width={0.98 * imagesContainerHeight * mainImage.aspect}
+                        />
+                    </div>
+                }
                 <div className={styles.imageContainer}>
                     <Image
                         style={{ objectFit: "contain" }}
