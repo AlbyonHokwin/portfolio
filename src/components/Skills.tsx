017e5028c03071c16bb92a45bb89a26cab3f46b9
Camille HAUSTANT <camillehaustant@gmail.com>
2023-01-29 16:19:17 +0100

    change style - remove some scrollbars

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import styles from '@/styles/Skills.module.css';
 import Skill from './elements/Skill';
+
 import type { skillType } from '@/types/skillType';
 
 type propsType = {
@@ -8,16 +9,30 @@ type propsType = {
 }
 
 function Skills({ skills }: propsType) {
+    const skillsContainerRef = useRef<HTMLDivElement>(null);
+    const [size, setSize] = useState<number>(0);
+    const numOfSkillOnRow = 3;
+
+    useEffect(() => {
+        if (skillsContainerRef.current) {
+            console.log(skillsContainerRef.current.clientWidth);
+            let calculatedSize = Math.min(Math.floor(0.85 * skillsContainerRef.current.clientWidth / numOfSkillOnRow), 100);
+            setSize(calculatedSize);
+        }
+    }, [skillsContainerRef]);
+
+
+
     return (
         <div className={styles.container}>
             <h2 className={styles.title}>Compétences</h2>
-            <div className={styles.skillsContainer}>
+            <div className={styles.skillsContainer} ref={skillsContainerRef}>
                 <div className={styles.skills}>
                     {skills.map((skill, i) => {
                         return (<Skill
                             key={i}
                             skill={skill}
-                            size={100}
+                            size={size}
                         />);
                     })}
                 </div>
