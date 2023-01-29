0f91445338a517285de7c82d26fd08f612012f56
Camille HAUSTANT <camillehaustant@gmail.com>
2023-01-29 16:28:24 +0100

    change width

--- src/components/modules/Project.tsx
+++ src/components/modules/Project.tsx
@@ -35,74 +35,80 @@ function Project({ project }: propsType) {
 
     return (
         <div className={styles.container}>
-            <div className={styles.images} ref={imagesContainerRef}>
-                {video &&
-                    <div className={styles.videoContainer}>
-                        <iframe
-                            className={styles.video}
-                            src={video}
-                            allow="autoplay; fullscreen"
-                            loading='lazy'
-                            height={0.98 * imagesContainerHeight}
-                            width={0.98 * imagesContainerHeight * mainImage.aspect}
-                        />
-                    </div>
-                }
-                <div className={styles.imageContainer}>
-                    <Image
-                        style={{ objectFit: "contain" }}
-                        className={styles.image}
-                        src={mainImage.url}
-                        alt={mainImage.alt}
-                        width={mainImage.width}
-                        height={mainImage.height}
-                    />
-                </div>
-                {images.map((image, i) => {
-                    return (
-                        <div key={i} className={styles.imageContainer}>
-                            <Image
-                                style={{ objectFit: "contain" }}
-                                className={styles.image}
-                                src={image.url}
-                                alt={image.alt}
-                                width={image.width}
-                                height={image.height}
-                            />
-                        </div>
-                    )
-                })}
-            </div>
             <div className={styles.titleContainer}>
                 <h3 className={styles.title}>{projectTitle}</h3>
                 {!onProgress && <time className={styles.date}>(Fini en {dateStr})</time>}
             </div>
-            <div className={styles.skills}>
-                {skills.map((skill, i) => {
-                    return (<Skill key={i} skill={skill} />);
-                })}
-            </div>
-
-            <div className={styles.description}>
-                <PortableText value={description} />
-            </div>
-
-            {githubLinks[0] && <div className={styles.linksContainer}>
-                <Image
-                    style={{ objectFit: "contain" }}
-                    src={'/github.png'}
-                    alt="Logo github"
-                    width={50}
-                    height={50}
-                />
-                <div className={styles.links}>
-                    {githubLinks.map((link, i) => {
+            <div className={styles.content}>
+                <div className={styles.images} ref={imagesContainerRef}>
+                    {video &&
+                        <div className={styles.videoContainer}>
+                            <iframe
+                                className={styles.video}
+                                src={video}
+                                allow="autoplay; fullscreen"
+                                loading='lazy'
+                                height={0.98 * imagesContainerHeight}
+                                width={0.98 * imagesContainerHeight * mainImage.aspect}
+                            />
+                        </div>
+                    }
+                    <div className={styles.imageContainer}>
+                        <Image
+                            style={{ objectFit: "contain" }}
+                            className={styles.image}
+                            src={mainImage.url}
+                            alt={mainImage.alt}
+                            width={mainImage.width}
+                            height={mainImage.height}
+                        />
+                    </div>
+                    {images.map((image, i) => {
                         return (
-                            <a key={i} href={link.url} className={styles.link}>{link.name}</a>
-                        );
+                            <div key={i} className={styles.imageContainer}>
+                                <Image
+                                    style={{ objectFit: "contain" }}
+                                    className={styles.image}
+                                    src={image.url}
+                                    alt={image.alt}
+                                    width={image.width}
+                                    height={image.height}
+                                />
+                            </div>
+                        )
                     })}
                 </div>
-            </div>}
+                {/* <div className={styles.titleContainer}>
+                <h3 className={styles.title}>{projectTitle}</h3>
+                {!onProgress && <time className={styles.date}>(Fini en {dateStr})</time>}
+            </div> */}
+                <div className={styles.skills}>
+                    {skills.map((skill, i) => {
+                        return (<Skill key={i} skill={skill} />);
+                    })}
+                </div>
+
+                <div className={styles.description}>
+                    <PortableText value={description} />
+                </div>
+
+                {githubLinks[0] && <div className={styles.linksContainer}>
+                    <Image
+                        style={{ objectFit: "contain" }}
+                        src={'/github.png'}
+                        alt="Logo github"
+                        width={50}
+                        height={50}
+                    />
+                    <div className={styles.links}>
+                        {githubLinks.map((link, i) => {
+                            return (
+                                <a key={i} href={link.url} className={styles.link}>{link.name}</a>
+                            );
+                        })}
+                    </div>
+                </div>}
+            </div>
         </div>
     )
 }
