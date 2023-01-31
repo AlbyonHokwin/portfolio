cbdbdfe82dcad39dd3e0b92b43392cf37064e5f5
Camille HAUSTANT <camillehaustant@gmail.com>
2023-01-31 10:47:59 +0100

    add env variables for sanity client

--- src/pages/api/sanityClient.ts
+++ src/pages/api/sanityClient.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const sanityClient = createClient({
-    projectId: "0klfxkkg",
-    dataset: "production",
-    apiVersion: "2023-01-16",
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_DATASET,
+    apiVersion: process.env.SANITY_API_VERSION,
     useCdn: false
 });
