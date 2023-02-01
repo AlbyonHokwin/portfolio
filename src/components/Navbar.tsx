b1f321791b6acb7bd5e62961d41b5ac39f8dcc2c
Camille HAUSTANT <camillehaustant@gmail.com>
2023-02-01 10:00:04 +0100

    integration of social links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,20 @@
 import React, { useState } from 'react'
 import Link from 'next/link';
+import Image from 'next/image';
 import styles from '@/styles/Navbar.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faBars, faX } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
+import type { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import type { socialType } from '@/types/socialType';
+
+type socialHashIconType = Record<string, IconDefinition>;
+
+const socialHashIcons: socialHashIconType = {
+    'GitHub': faGithub,
+    'LinkedIn': faLinkedin,
+}
 
 type propsType = {
     socials: socialType[];
@@ -21,22 +31,32 @@ function Navbar({ socials }: propsType) {
                 <FontAwesomeIcon icon={faHome} className={styles.homeIcon} />
             </Link>
 
-            <div className={`${styles.menuSection} ${showMenu && styles.open}`}>
-                <ul>
-                    <li>
-                        <Link href="#projects">Projets</Link>
-                    </li>
-                    <li>
-                        <Link href="#experiences">Expériences</Link>
-                    </li>
-                    <li>
-                        <Link href="#skills">Compétences</Link>
-                    </li>
-                    <li>
-                        <Link href="#contact">Contact</Link>
-                    </li>
-                </ul>
-            </div>
+            <ul className={styles.socials}>
+                {socials.map((social, i) => {
+                    return (
+                        <li key={i}>
+                            <a href={social.url}>
+                                <FontAwesomeIcon icon={socialHashIcons[social.name]} className={styles.socialIcon} />
+                            </a>
+                        </li>
+                    )
+                })}
+            </ul>
+
+            <ul className={`${styles.menuSection} ${showMenu && styles.open}`}>
+                <li>
+                    <Link href="#projects">Projets</Link>
+                </li>
+                <li>
+                    <Link href="#experiences">Expériences</Link>
+                </li>
+                <li>
+                    <Link href="#skills">Compétences</Link>
+                </li>
+                <li>
+                    <Link href="#contact">Contact</Link>
+                </li>
+            </ul>
 
             <FontAwesomeIcon
                 icon={showMenu ? faX : faBars}
