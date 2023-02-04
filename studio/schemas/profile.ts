f26770fe283dbc9ff5f0766088319adbc716a58d
Camille HAUSTANT <camillehaustant@gmail.com>
2023-02-04 18:27:08 +0100

    modify profile schema by changing description to rich text, and add picture for about me page

--- studio/schemas/profile.ts
+++ studio/schemas/profile.ts
@@ -7,10 +7,16 @@ export default defineType({
     fields: [
         defineField({ name: 'firstname', type: 'string', title: 'Prénom' }),
         defineField({ name: 'lastname', type: 'string', title: 'Nom' }),
-        defineField({ name: 'description', type: 'text', title: 'Description' }),
+        defineField({
+            name: 'description',
+            title: 'Description',
+            type: 'array',
+            of: [defineArrayMember({ type: 'block' })]
+        }),
         defineField({ name: 'email', type: 'string', title: 'Adresse e-mail' }),
         defineField({ name: 'phone', type: 'string', title: 'Numéro de téléphone' }),
         defineField({ name: 'picture', type: 'accessibleImage', title: 'Image de profil' }),
         defineField({ name: 'pictureGit', type: 'accessibleImage', title: 'Image de profil sur github' }),
+        defineField({ name: 'aboutMePicture', type: 'accessibleImage', title: 'Image pour la page "à propos de moi"' }),
     ]
 })
