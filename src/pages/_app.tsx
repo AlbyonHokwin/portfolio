047bed1cf02293c8c4f1861d36086d83a22d1ccd
Camille HAUSTANT <camillehaustant@gmail.com>
2023-02-09 19:37:02 +0100

    implement vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react';
+
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
